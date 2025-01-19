12ae36b2b287e7e0ff04ed9d6cd67af272bd3be4
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2025-01-20 00:01:47 +0530

    Update BrandName component to link to Twinverse website and adjust container width in index.html
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- components/brand.tsx
+++ components/brand.tsx
@@ -1,7 +1,7 @@
 export function BrandName() {
   return (
     <div className='text-2xl font-bold tracking-wider text-black dark:text-white'>
-      Twinverse
+      <a href='https://twinverse.in' target="_blank">Twinverse</a>
       <span className='bg-white dark:bg-black text-black dark:text-white px-1'>
         ™
       </span>
