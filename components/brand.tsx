c4712e003e12399ade043ed5ba9acc1725891acc
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2025-01-23 00:32:23 +0530

    Refactor BrandName component to use span instead of div for improved inline styling
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- components/brand.tsx
+++ components/brand.tsx
@@ -1,10 +1,10 @@
 export function BrandName() {
   return (
-    <div className='text-2xl font-bold tracking-wider text-black dark:text-white'>
+    <span className='inline-block text-2xl font-bold tracking-wider text-black dark:text-white'>
       <a href='https://twinverse.in' target="_blank">Twinverse</a>
       <span className='bg-white dark:bg-black text-black dark:text-white px-1'>
         ™
       </span>
-    </div>
+    </span>
   );
 }
