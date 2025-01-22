04ad7dd1bc26fd598f0bae479d473e6b57fedf22
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2025-01-23 01:18:42 +0530

    Add Open Graph and Twitter meta tags for improved social sharing
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- app/routes/trial.tsx
+++ app/routes/trial.tsx
@@ -5,9 +5,43 @@ export function meta({}: Route.MetaArgs) {
   return [
     { title: 'TwinTry™' },
     {
-      name: 'Virtual Try on Experience',
+      name: 'description',
       content:
         'Provides high quality and efficient virtual try on for all at your comfort.',
+    },
+    {
+      name: 'og:title',
+      content: 'TwinTry™ - Virtual Try on Experience',
+    },
+    {
+      name: 'og:description',
+      content:
+        'Experience high quality and efficient virtual try on for all at your comfort.',
+    },
+    {
+      name: 'og:image',
+      content: '/tryon-preview.png',
+    },
+    {
+      name: 'og:type',
+      content: 'website',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:title',
+      content: 'TwinTry™ - Virtual Try on Experience',
+    },
+    {
+      name: 'twitter:description',
+      content:
+        'Experience high quality and efficient virtual try on for all at your comfort.',
+    },
+    {
+      name: 'twitter:image',
+      content: '/tryon-preview.png',
     },
   ];
 }
